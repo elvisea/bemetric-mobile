177c5bf0b2fad62647dbb52621d87438e2d5ee58
Elvis <eleram@protonmail.com>
2024-02-16 15:05:14 -0300

    Adjusted point of interest

--- src/screens/AppScreens/Markers/screens/Point/types/index.ts
+++ src/screens/AppScreens/Markers/screens/Point/types/index.ts
@@ -1,3 +1,8 @@
+import { Region } from "react-native-maps";
+
+import { Responses } from "@typings/index";
+import { Coord } from "@screens/AppScreens/Markers/types";
+
 interface IPointsInterest {
   incluir: boolean;
   codigoCliente: number;
@@ -11,4 +16,45 @@ interface IPointsInterest {
   alertaPermanenciaTempo?: number;
 }
 
-export { IPointsInterest };
+type PointReceived = {
+  alertaEntradaSaida: boolean;
+  alertaPermanencia: boolean;
+  alertaPermanenciaTempo?: number;
+  codigoCliente: number;
+  codigoParceiro: number;
+  codigoPontoInteresse: number;
+  descricao: string;
+  latitude: number;
+  longitude: number;
+  nomePonto: string;
+  raio: number;
+};
+
+type NormalizedPoint = {
+  alert: boolean;
+  permanence: boolean;
+  duration?: number;
+  client: number;
+  partner: number;
+  code: number;
+  description: string;
+  coord: Coord;
+  name: string;
+  radius: number;
+};
+
+type Params = {
+  codigoPontoInteresse: number;
+};
+
+type State = {
+  point: NormalizedPoint | null;
+  updatedPoint: NormalizedPoint;
+  initialRegion: Region | undefined;
+  isOpenModal: boolean;
+  isLoading: boolean;
+  isEditMode: boolean;
+  responses: Record<"update" | "delete", Responses>;
+};
+
+export { IPointsInterest, PointReceived, NormalizedPoint, Params, State };
